46cd64582a5cf2c108b01ae4c9679d490902e51b
his-0203 <heesoo0203@gmail.com>
2024-07-10 22:33:38 +0900

    initail commit

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from ai_utils import generate_movie_quote_text, generate_movie_quote_image
+from api.ai_utils import generate_movie_quote_text, generate_movie_quote_image
 
 app = Flask(__name__)
 
